d5089bb6f577188ccada18ecbf1ea880be38f2fe
edangolgithub <dangolevan@gmail.com>
2018-06-18 10:34:38 +0545

    msg

--- ribeshanish/Program.cs
+++ ribeshanish/Program.cs
@@ -27,32 +27,110 @@ namespace ribeshanish
 
             //} while (i >= 1);
 
-            int prime = 19;
-            bool isprime = false;
-            
-            for(int i=2; i<=prime-1; i++)
-            {
-                if(prime%i==0)
-                {
-                    isprime = false;
-                    break;
-                }
-                else
-                {
-                    isprime = true;
-                }
+            //int prime = 19;
+            //bool isprime = false;
+
+            //for(int i=2; i<=prime-1; i++)
+            //{
+            //    if(prime%i==0)
+            //    {
+            //        isprime = false;
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        isprime = true;
+            //    }
+            //}
+            //if(isprime==true)
+            //{
+            //    Console.WriteLine("prime");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not prime");
+            //}
+
+            int a = 32;
+
+            //if(a==1)
+            //{
+            //    Console.WriteLine("if");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("else");
+            //}
+
+            //switch(a)
+            //{
+            //    case 1:
+            //        Console.WriteLine("if");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("ifif");
+            //        break;
+            //    case 32:
+            //        Console.WriteLine("32");
+            //        break;
+            //    default:
+            //        Console.WriteLine("else");
+            //        break;
+            //}
+
+            ////string grade = "A";
+
+            ////switch(grade)
+            ////{
+            ////    case "A":
+            ////        Console.WriteLine("excellent");
+            ////        break;
+
+            ////    case "B":
+            ////        Console.WriteLine("very good");
+            ////        break;
+
+            ////    case "C":
+            ////        Console.WriteLine("good");
+            ////        break;
+
+            ////    case "D":
+            ////        Console.WriteLine("satisfactory");
+            ////        break;
+
+            ////    case "E":
+            ////        Console.Write("very poor");
+            ////        break;
+
+            ////    case "N":
+            ////        Console.WriteLine("fail");
+            ////        break;
+
+            //    default:
+
+            //        Console.WriteLine("begin the class again");
+            //        break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             }
-            if(isprime==true)
-            {
-                Console.WriteLine("prime");
-            }
-            else
-            {
-                Console.WriteLine("not prime");
-            }
+
+    
+           
         }
 
 
        
     }
-}
+
