1b7946d389483fdf4218dcb693e09d327b429264
edangolgithub <dangolevan@gmail.com>
2018-06-27 09:29:02 +0545

    push

--- ribeshanish/Program.cs
+++ ribeshanish/Program.cs
@@ -21,7 +21,7 @@ namespace ribeshanish
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new evan.EvanDrawing());
+            Application.Run(new evan.evanform());
 
 
     
