2cd08ccb48ede49dfad75536f5fbd15ae9fc5be0
edangolgithub <dangolevan@gmail.com>
2018-06-27 10:53:39 +0545

    com

--- RibeshAnishConsole/Program.cs
+++ RibeshAnishConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RibeshAnishConsole.evan;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,8 @@ namespace RibeshAnishConsole
     {
         static void Main(string[] args)
         {
+            ArrayLearning al = new ArrayLearning();
+            al.showarray();
         }
     }
 }
---
+++ RibeshAnishConsole/evan/ArrayLearning.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RibeshAnishConsole.evan
+{
+    class ArrayLearning
+    {
+       
+        int[] arr = { 1, 2, 3, 4, 8 };
+
+        public void showarray()
+        {
+        
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+           
+        }
+
+    }
+}
