b04ce73e947481c77742e3ac1712bdffd9572056
anishadhikarigithub <adhikarianish423@gmail.com>
2018-06-27 10:05:13 +0545

    modified

---
+++ RibeshAnishConsole/Anish/ArrayLearning.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RibeshAnishConsole.Anish
+{
+    class ArrayLearning
+    {
+        string[] arr = { "a", "s", "e" };
+
+        public void Showarray()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(arr[i]);
+                
+            }
+        }
+
+        
+    }
+}
--- RibeshAnishConsole/Program.cs
+++ RibeshAnishConsole/Program.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using RibeshAnishConsole.Anish;
 namespace RibeshAnishConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ArrayLearning anis = new ArrayLearning();
+            anis.Showarray();
         }
     }
 }
