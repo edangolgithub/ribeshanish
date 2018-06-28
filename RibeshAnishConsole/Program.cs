8a7e6e51a9f4424316cc48f3fa7557938f75503a
edangolgithub <dangolevan@gmail.com>
2018-06-28 09:41:58 +0545

    changes

--- RibeshAnishConsole/Program.cs
+++ RibeshAnishConsole/Program.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using RibeshAnishConsole.Anish;
+using RibeshAnishConsole.evan;
 namespace RibeshAnishConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ArrayLearning anis = new ArrayLearning();
-            anis.Showarray();
+            ArrayLearning a = new ArrayLearning();
+            //a.findproduct();
+            a.reversearray();
         }
     }
 }
--- RibeshAnishConsole/evan/ArrayLearning.cs
+++ RibeshAnishConsole/evan/ArrayLearning.cs
@@ -8,16 +8,108 @@ namespace RibeshAnishConsole.evan
     class ArrayLearning
     {
        
-        int[] arr = { 1, 2, 3, 4, 8 };
+        int[] firstarray = { 1, 2, 3, 4, 8 };
 
+        int[] secondarray;
         public void showarray()
         {
         
             for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine(arr[i]);
+                Console.WriteLine(firstarray[i]);
             }
            
+        }
+
+        public void search(int num)
+        {
+            bool flag = false;
+            for (int i = 0; i < firstarray.Length; i++)
+            {
+                if(num==firstarray[i])
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if(flag==true)
+            {
+                Console.WriteLine("exist");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+
+        public void findindex(int num)
+        {
+            
+            for (int i = 0; i < firstarray.Length; i++)
+            {
+                if (num == firstarray[i])
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+
+            
+        }
+
+        public void findaverage()
+        {
+            
+            double sum = 0;
+            for (int i = 0; i < firstarray.Length; i++)
+            {
+                sum = sum + firstarray[i];
+            }
+            double n = firstarray.Length;
+            double ave = sum / n;
+            Console.WriteLine(ave);
+        }
+
+        public void findproduct()
+        {
+            double product = 1;
+
+            for (int i = 0; i < firstarray.Length; i++)
+            {
+                product = product * firstarray[i];
+            }
+            Console.WriteLine(product);
+        }
+
+        public void reversearray()
+        {
+            secondarray = new int[firstarray.Length];
+
+            for (int i = 0; i < firstarray.Length; i++)
+            {
+                secondarray[i] = firstarray[i];
+            }
+
+
+            for (int i = 0; i < secondarray.Length; i++)
+            {
+                Console.WriteLine(secondarray[i]);
+            }
+
+            int a = 0;
+            int b = firstarray.Length - 1;
+            for (int i = 0; i < secondarray.Length; i++)
+            {
+                secondarray[a] = firstarray[b];
+                a++;
+                b--;
+            }
+
+            for (int i = 0; i < secondarray.Length; i++)
+            {
+                Console.WriteLine(secondarray[i]);
+            }
         }
 
     }
