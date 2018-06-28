af97fc79e37c43ee30f72e12e25d26a453cd9763
edangol <edangol@EVAN1>
2018-06-28 09:46:43 +0545

    k

--- RibeshAnishConsole/Program.cs
+++ RibeshAnishConsole/Program.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using RibeshAnishConsole.Anish;
+using RibeshAnishConsole.ribesh;
 namespace RibeshAnishConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ArrayLearning anis = new ArrayLearning();
-            anis.Showarray();
+            ArrayLearning a = new ArrayLearning();
+            a.findproduct();
         }
     }
 }
