190828326131f7aee8bb3abf8ab3d7e803c94f07
edangolgithub <dangolevan@gmail.com>
2018-07-03 09:58:53 +0545

    arr

--- RibeshAnishConsole/Program.cs
+++ RibeshAnishConsole/Program.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using RibeshAnishConsole.ribesh;
+using RibeshAnishConsole.evan;
 namespace RibeshAnishConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Maths m = new Maths();
-            m.even();
-            //h today hora
-          
+            int[] arr = { 2, 5, 1, 72, 98 };
+            int[] arrr = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                arrr[i] = 4+i;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arrr[i]);
+            }
         }
     }
 }
