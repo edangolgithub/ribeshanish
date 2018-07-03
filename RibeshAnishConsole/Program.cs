fd7f33da2e93a44c861eed3e7d629035f40280ca
edangol <edangol@EVAN1>
2018-07-03 10:07:29 +0545

    o

--- RibeshAnishConsole/Program.cs
+++ RibeshAnishConsole/Program.cs
@@ -19,6 +19,21 @@ namespace RibeshAnishConsole
             {
                 Console.WriteLine(arrr[i]);
             }
+            array();
+        }
+
+        public static void array()
+        {
+            int[] arr = { 1, 3, 5, 7, 9 };
+            int[] arrr = new int[7];
+            for(int i=0; i<7; i++)
+            {
+                arrr[i] = 5 + i;
+            }
+            for(int i=0; i<7; i++)
+            {
+                Console.WriteLine(arrr[i]);
+            }
         }
     }
 }
