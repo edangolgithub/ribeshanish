8fd7ec3eb168337e14680624b9d21a074fedc73a
edangol <edangol@EVAN1>
2018-07-06 08:30:40 +0545

    encapsulation

--- RibeshAnishConsole/Program.cs
+++ RibeshAnishConsole/Program.cs
@@ -3,37 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using RibeshAnishConsole.evan;
+using RibeshAnishConsole.ribesh;
+
 namespace RibeshAnishConsole
 {
     class Program
     {
-        static void nMain(string[] args)
+        static void uMain(string[] args)
         {
-            int[] arr = { 2, 5, 1, 72, 98 };
-            int[] arrr = new int[5];
-            for (int i = 0; i < 5; i++)
-            {
-                arrr[i] = 4+i;
-            }
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(arrr[i]);
-            }
-            //array(); 
+            Constructor c = new Constructor(8); 
         }
 
         public static void array()
         {
-            int[] arr = { 1, 3, 5, 7, 9 };
-            int[] arrr = new int[7];
-            for(int i=0; i<7; i++)
-            {
-                arrr[i] = 5 + i;
-            }
-            for(int i=0; i<7; i++)
-            {
-                Console.WriteLine(arrr[i]);
-            }
+
         }
+
     }
 }
