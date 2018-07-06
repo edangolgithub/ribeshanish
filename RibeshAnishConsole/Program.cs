67d00ff27bfe8fd0cfea9915c3bea32eacc6f887
edangol <edangol@EVAN1>
2018-07-06 09:49:32 +0545

    inheritance

--- RibeshAnishConsole/Program.cs
+++ RibeshAnishConsole/Program.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using EvanDangol.Tutorial.General;
-using RibeshAnishConsole.evan;
+using RibeshAnishConsole.ribesh;
 namespace RibeshAnishConsole
 {
     class Program
     {
+
+        enum days { sun ,mon ,tue ,wed ,thur ,fri ,sat}
         static void Main(string[] args)
         {
-            encap e = new encap();
-            e.setname("ra");
-            e.getname();
-            e.setage(14);
-            e.getage();
+            square s = new square();
+            s.length = 4;
+            s.calculatearea();
+
+            rectangle r = new rectangle();
+            r.length = 25;
+            r.breadth = 20;
+            r.calculatearea();
+
+            triangle t = new triangle();
+            t.length = 15;
+            t.breadth = 10;
+            t.calculatearea();
+
+            Shape ss = new Shape();
+            ss.length = 20;
+            ss.breadth = 30;
+            ss.calculatearea();
         }
+
+       
 
         
     }
