0e3b14d03f2e5bc3b07f7c292082cf09372d89a4
evan dangol <dangolevan@gmail.com>
2018-07-12 09:09:18 +0545

    web

--- RibeshAnishConsole/Program.cs
+++ RibeshAnishConsole/Program.cs
@@ -12,24 +12,8 @@ namespace RibeshAnishConsole
         enum days { sun, mon,tue,wed,thu,fri, sat};
         static void Main(string[] args)
         {
-            Square s = new Square();
-            s.length = 2;
-            s.calculatearea();
-
-            Rectangle r = new Rectangle();
-            r.length = 40;
-            r.breadth = 20;
-            r.calculatearea();
-
-            Triangle t = new Triangle();
-            t.length = 21;
-            t.breadth = 13;
-            t.calculatearea();
-
-            Shape ss = new Shape();
-            ss.length = 30;
-            ss.breadth = 50;
-            ss.calculatearea();
+            win w = new win();
+            w.ShowDialog();
         }
 
         
---
+++ RibeshAnishConsole/evan/win.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RibeshAnishConsole.evan
+{
+    class win :Form
+    {
+        Button b = new Button();
+        TextBox t = new TextBox();
+        public win()
+        {
+            b.Text = "this is ribesh button";
+            
+          
+
+            this.Controls.Add(b);
+            this.Controls.Add(t);
+        }
+
+    }
+}
